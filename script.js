8966eead330863159b1fcd52e6f873eace14654b
brothers08 <45543828+brothers08@users.noreply.github.com>
2019-03-08 14:13:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,32 @@ $(menuBarButton).click(function(){
   $(menuBar).toggle();
   menuBar.style.textAlign = 'center';
 });
+
+//Make Image Appear when scrolling
+function showImages(el) {
+  var windowHeight = jQuery( window ).height();
+  $(el).each(function(){
+      var thisPos = $(this).offset().top;
+
+      var topOfWindow = $(window).scrollTop();
+      if (topOfWindow + windowHeight - 200 > thisPos ) {
+          $(this).addClass("fadeIn");
+      }
+  });
+}
+
+// if the image in the window of browser when the page is loaded, show that image
+$(document).ready(function(){
+      showImages('.home-image1');
+      showImages('.home-image2');
+      document.getElementById('column').style.visibility = 'visible';
+      document.getElementById('column2').style.visibility = 'visible';
+});
+
+// if the image in the window of browser when scrolling the page, show that image
+$(window).scroll(function() {
+      showImages('.home-image1');
+      showImages('.home-image2');
+      document.getElementById('column').style.visibility = 'visible';
+      document.getElementById('column2').style.visibility = 'visible';
+});
